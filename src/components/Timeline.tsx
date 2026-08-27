0b3d23eff1dc60ff056ef24872b9f0ce7efb8206
Leonardo Aleixo <leonardo.aleixo@aluno.ifsp.edu.br>
2026-08-27 18:28:50 -0300

    feat: seção de linha do tempo
    WIP: criação dos arquivos e estrutura inicial da linha do tempo

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,52 @@
+const Events = () => {
+    const events = [
+    {
+        id: 1,
+        content: "Idealização e início do projeto",
+        date: "3/2024",
+        image: "none",
+    },
+    {
+        id: 2,
+        content: "Criação do MVP (Prancha única)",
+        date: "6/2024",
+        image: "none",
+    },
+    {
+        id: 3,
+        content: "Apresentação em congressos",
+        date: "3/2025",
+        image: "none",
+    },
+    {
+        id: 4,
+        content: "Lançamento do Website",
+        date: "10/2025",
+        image: "none",
+    },
+    {
+        id: 5,
+        content: "Lançamento do App versão Web",
+        date: "11/2025",
+        image: "none",
+    },
+    {
+        id: 6,
+        content: "Parceria com a Prefeitura de Jacareí",
+        date: "3/2026",
+        image: "none",
+    },
+    {
+        id: 7,
+        content: "Publicação do aplicativo na Play Store",
+        date: "5/2026",
+        image: "none",
+    },
+    {
+        id: 8,
+        content: "Expansão da Equipe",
+        date: "6/2026",
+        image: "none",
+    },
+    ];
+}
